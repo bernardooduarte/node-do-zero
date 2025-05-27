3469a9b9bb20e70a11fc4ea4d16312abd716b906
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-05-27 17:47:14 -0300

    adicionando arquivos

---
+++ server.js
@@ -0,0 +1,70 @@
+// import { createServer } from 'node:http'
+
+// const server = createServer((request, response) => {
+//     response.write('Hello World')
+
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+// import { DatabaseMemory } from './database-memory.js'
+import { DatabasePostgres } from './database-postgres.js'
+
+const server = fastify()
+
+const database = new DatabasePostgres()
+
+server.post('/videos', async (request, reply) => {
+    const { title, description, duration } = request.body
+
+    console.log(request.body)
+
+   await database.create({
+        title,
+        description,
+        duration,
+    })
+
+    console.log(database.list())
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', async (request) => {
+    const search = request.query.search
+
+    console.log(search)
+
+    const videos = await database.list(search)
+
+    console.log(videos)
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+    const video = database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: 3333,
+})
+
